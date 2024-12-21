a4669657c262ec3d25cb418c9f7a41f035ad1681
sriyazhininandakumar <158152070+sriyazhininandakumar@users.noreply.github.com>
2024-12-22 03:19:58 +0530

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
@@ -7,40 +7,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose
-    .connect('mongodb://localhost:27017/mern-app')
-    .then(() => {
-        console.log('DB Connected');
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+// In-memory todo list (instead of MongoDB)
+let todos = [];
 
-
-
-const todoSchema = new mongoose.Schema({
-    todolist: {
-        required: true,
-        type: String,
-    },
-    isComplete: {
-        type: Boolean,
-        default: false,
-    },
-    id: {        
-        type: Date,
-        default: Date.now,
-    }
-});
-
-
-const todoModel = mongoose.model('Todo', todoSchema);
-
-app.post('/todos', async (req, res) => {
+// POST route to add a new todo
+app.post('/todos', (req, res) => {
     const { todolist } = req.body;
     try {
-        const newTodo = new todoModel({ todolist });
-        await newTodo.save();
+        const newTodo = {
+            todolist,
+            isComplete: false,
+            id: new Date().toISOString(), // Use a string as an ID (e.g., timestamp)
+        };
+        todos.push(newTodo);
         res.status(201).json(newTodo);
     } catch (error) {
         console.log(error);
@@ -48,10 +27,9 @@ app.post('/todos', async (req, res) => {
     }
 });
 
-
-app.get('/todos', async (req, res) => {
+// GET route to retrieve all todos
+app.get('/todos', (req, res) => {
     try {
-        const todos = await todoModel.find();
         res.json(todos);
     } catch (error) {
         console.log(error);
@@ -59,20 +37,20 @@ app.get('/todos', async (req, res) => {
     }
 });
 
-app.put('/todos/:id', async (req, res) => {
+// PUT route to update a todo
+app.put('/todos/:id', (req, res) => {
     try {
         const { todolist, isComplete } = req.body;
-        const id = req.params.id;
+        const { id } = req.params;
 
-        const updatedTodo = await todoModel.findByIdAndUpdate(
-            id,
-            { todolist, isComplete },
-            { new: true }
-        );
+        let updatedTodo = todos.find((todo) => todo.id === id);
 
         if (!updatedTodo) {
             return res.status(404).json({ message: 'Todo not found' });
         }
+
+        updatedTodo.todolist = todolist !== undefined ? todolist : updatedTodo.todolist;
+        updatedTodo.isComplete = isComplete !== undefined ? isComplete : updatedTodo.isComplete;
 
         res.json(updatedTodo);
     } catch (error) {
@@ -81,16 +59,17 @@ app.put('/todos/:id', async (req, res) => {
     }
 });
 
-
-app.delete('/todos/:id', async (req, res) => {
+// DELETE route to remove a todo
+app.delete('/todos/:id', (req, res) => {
     try {
-        const id = req.params.id;
-        const deletedTodo = await todoModel.findByIdAndDelete(id);
+        const { id } = req.params;
+        const todoIndex = todos.findIndex((todo) => todo.id === id);
 
-        if (!deletedTodo) {
+        if (todoIndex === -1) {
             return res.status(404).json({ message: 'Todo not found' });
         }
 
+        todos.splice(todoIndex, 1);
         res.status(204).end();
     } catch (error) {
         console.log(error);
